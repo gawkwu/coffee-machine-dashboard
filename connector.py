f7744a7aae71a30a042d198b8d8823b9c94a2a6c
gawkwu <gawkwu@gmail.com>
2021-03-12 21:14:23 +0800

    Debug

--- connector.py
+++ connector.py
@@ -10,28 +10,28 @@ DATABASE_NAME = 'coffeemachine.db'
 DATABASE_TABLES = ['machine_order', 'machine_state']
 
 
-class SQLiteConnector:
-    def __init__(self, db_name):
-        self.db_name = db_name
-
-    def read_sql_table(self, table_name, index_col=None, columns=None):
-        conn, result = None, None
-        try:
-            conn = sqlite3.connect(self.db_name)
-            conn.row_factory = sqlite3.Row
-            cur = conn.cursor()
-            columns = '*' if columns is None else columns
-            query = f'SELECT {columns} FROM {table_name}'
-            cur.execute(query)
-            index_col = cur.fetchone().keys() if index_col is None else index_col
-            result = pd.DataFrame(data=cur.fetchall(), columns=index_col)
-            cur.close()
-        except Exception as error:
-            print(error)
-        finally:
-            if conn is not None:
-                conn.close()
-            return result
+# class SQLiteConnector:
+#     def __init__(self, db_name):
+#         self.db_name = db_name
+#
+#     def read_sql_table(self, table_name, index_col=None, columns=None):
+#         conn, result = None, None
+#         try:
+#             conn = sqlite3.connect(self.db_name)
+#             conn.row_factory = sqlite3.Row
+#             cur = conn.cursor()
+#             columns = '*' if columns is None else columns
+#             query = f'SELECT {columns} FROM {table_name}'
+#             cur.execute(query)
+#             index_col = cur.fetchone().keys() if index_col is None else index_col
+#             result = pd.DataFrame(data=cur.fetchall(), columns=index_col)
+#             cur.close()
+#         except Exception as error:
+#             print(error)
+#         finally:
+#             if conn is not None:
+#                 conn.close()
+#             return result
 
 
 def connect(table_name, index_col=None, columns=None):
@@ -41,6 +41,7 @@ def connect(table_name, index_col=None, columns=None):
         print('Connecting to PostgreSQL database server...')
         conn = psycopg2.connect(db_url, sslmode='require')
         cur = conn.cursor()
+        columns = '*' if columns is None else columns
         query = f'SELECT {columns} FROM {table_name}'
         cur.execute(query)
         index_col = cur.fetchone().keys() if index_col is None else index_col
